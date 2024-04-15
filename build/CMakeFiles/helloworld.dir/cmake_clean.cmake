4140730ddab39db16d8f7c5795c45b446db6ff81
Albaro Pereyra <AlbaroPereyra22@gmail.com>
2024-04-14 19:29:23 -0700

    Initial commit

---
+++ build/CMakeFiles/helloworld.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloworld.dir/controllers/cookies.cc.o"
+  "CMakeFiles/helloworld.dir/controllers/cookies.cc.o.d"
+  "CMakeFiles/helloworld.dir/main.cc.o"
+  "CMakeFiles/helloworld.dir/main.cc.o.d"
+  "helloworld"
+  "helloworld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
